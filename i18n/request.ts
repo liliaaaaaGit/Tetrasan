9e753c8b1e5db2c4b541b272667d46de0d1b2419
liliaaaaaGit <lilia.schraut@icloud.com>
2025-11-10 22:14:55 +0100

    Fix next-intl request config import

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,9 @@
-import { defineRequestConfig } from "next-intl/server";
+import { getRequestConfig } from "next-intl/server";
 
 import { getRequestLocale } from "./get-request-locale";
 import { getMessagesWithFallback } from "./get-messages";
 
-export default defineRequestConfig(async () => {
+export default getRequestConfig(async () => {
   const locale = await getRequestLocale();
   const messages = await getMessagesWithFallback(locale);
 
